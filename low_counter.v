fabaddb74107debff8d88ec620d387b8b692d1bb
Sherzod Mamadaliev <mamadaliev@yahoo.com>
2019-06-07 11:43:56 +0300

    fix(low_counter.v): coding conventions and input
    change the input name clock to in

--- low_counter.v
+++ low_counter.v
@@ -1,21 +1,21 @@
 module low_counter
-#(parameter N=26, DIV = 25'd25000000)
-(input wire clock,
-output reg out);
+    #(parameter N=26, DIV = 25'd25000000)
+    (input wire in,
+    output reg out);
 
-reg [N:0] gen2;
-wire[N:0] gen2_next;
-wire counter_next;
+    reg [N:0] gen2;
+    wire[N:0] gen2_next;
+    wire counter_next;
 
-always@(posedge clock)
-begin
-    gen2 <= gen2_next;
-	out <= out;
-	if (gen2 == DIV) begin
-		gen2 <= 0;
-		out <= ~out;
-	end
-end
+        always@(posedge in)
+    begin
+        gen2 <= gen2_next;
+        out <= out;
+        if (gen2 == DIV) begin
+            gen2 <= 0;
+            out <= ~out;
+        end
+    end
 
-assign gen2_next=gen2 + 1'b1;
+    assign gen2_next=gen2 + 1'b1;
 endmodule
